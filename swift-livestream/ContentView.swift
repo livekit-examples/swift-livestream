4999eeac7c55598277cd9ab3f05e7ef6325b8e4e
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2023-06-02 20:21:44 +0900

    project

---
+++ swift-livestream/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  swift-livestream
+//
+//  Created by Hiroshi Horie on 2023/06/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift-livestream/swift_livestreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_livestreamApp.swift
+//  swift-livestream
+//
+//  Created by Hiroshi Horie on 2023/06/02.
+//
+
+import SwiftUI
+
+@main
+struct swift_livestreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
