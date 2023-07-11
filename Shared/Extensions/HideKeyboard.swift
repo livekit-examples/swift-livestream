6dd0a6e2e8bbfcd1d252d7db71a5a197505174c1
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2023-07-11 11:03:29 +0800

    fix macOS compile

--- Shared/Extensions/HideKeyboard.swift
+++ Shared/Extensions/HideKeyboard.swift
@@ -1,9 +1,10 @@
 import SwiftUI
 
-#if canImport(UIKit)
 extension View {
+
     func hideKeyboard() {
+        #if canImport(UIKit)
         UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+        #endif
     }
 }
-#endif
